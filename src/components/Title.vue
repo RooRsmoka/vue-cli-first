c859e2ba34b30ccf86c5ec87e0102c92ab6d81ef
RooRsmoka <chri.disa96@gmail.com>
2022-01-26 17:11:13 +0100

    deleted HelloWorld component, created Title component and updated App.Vue

---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 class="heading">La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body {
+    background-color: #ddd;
+}
+
+.heading {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: 46px;
+}
+</style>
